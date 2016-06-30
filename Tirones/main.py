ef1c7e9cef5fa67c3715d08d2707064ca5225606
patny205 <patny205@student.liu.se>
2016-06-30 14:33:57 +0200

    Plot will now check dates, plot will also work on arbitrary number of lines.

--- Tirones/main.py
+++ Tirones/main.py
@@ -3,8 +3,8 @@ import stock
 def main():
 
     ABB = stock.stock('ABB.ST')
-    ABB.getHist('2016-06-01', '2016-06-26')
-    ABB.plot('2016-06-01', '2016-06-26')
+    ABB.getHist('2016-05-01', '2016-06-26')
+    ABB.plot('2016-05-01', '2016-06-26',['Close','High','Low'],['b-','r--','r--'])
 
 if __name__ == '__main__':
     main()
--- Tirones/stock.py
+++ Tirones/stock.py
@@ -5,7 +5,7 @@ import matplotlib.dates as dat
 import datetime
 
 class stock:
-    """docstring for stock"""
+    """Class stock, contains all data for the stock-company"""
 
     def __init__(self, stockSymbol):
         #super(stock, self).__init__()
@@ -14,19 +14,39 @@ class stock:
     def getHist(self, start, end):
         self.yahooHandle = yahoo_finance.Share(self.symbol)
         self.hist = self.yahooHandle.get_historical(start, end)
+        self.start = start
+        self.end = end
 
         #self.closings = [day['Close'] for day in hist]
 
-    def plot(self, start, end, type='Close'):
+    def checkDates(self, start, end):
 
-        xvalue = [day[type] for day in self.hist]
-        dates = [datetime.datetime.strptime(day['Date'],"%Y-%m-%d") for day in self.hist]
+        if datetime.datetime.strptime(self.start,"%Y-%m-%d") > datetime.datetime.strptime(start,"%Y-%m-%d"):
+            raise ValueError("Start time before history start",'startbeforehist')
+        elif datetime.datetime.strptime(self.end,"%Y-%m-%d") < datetime.datetime.strptime(end,"%Y-%m-%d"):
+            raise ValueError("End time after history end",'endafterhist')
 
-        days = dat.DayLocator(tz=None, bymonthday=None, interval=2)
-        daysFmt = dat.DateFormatter("%d-%m-%y")
+
+    def plot(self, start, end, types=['Close'], fmt=['b-']):
+
+        self.checkDates(start, end)
+
+        if len(types) != len(fmt):
+            raise ValueError("Length of 'types' and 'fmt' must be the same. len(types): %d, len(fmt): %d" % (len(types), len(fmt)))
 
         fig, ax = plt.subplots()
-        ax.plot_date(dates, xvalue, fmt='b-')
+
+        for index, type in enumerate(types):
+
+            # TODO (BUG): Will always plot entire history
+            xvalue = [day[type] for day in self.hist]
+            dates = [datetime.datetime.strptime(day['Date'],"%Y-%m-%d") for day in self.hist]
+
+            days = dat.DayLocator(tz=None, bymonthday=None, interval=2)
+            daysFmt = dat.DateFormatter("%d-%m-%y")
+
+            ax.plot_date(dates, xvalue, fmt[index])
+
 
         # TODO: Make adjustable depending on range
         ax.xaxis.set_major_locator(days)
